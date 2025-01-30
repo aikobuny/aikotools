ce350e5a83003b26b9d8e1be95bdb4264e8347d1
Aiko <59002254+aikobuny@users.noreply.github.com>
2025-01-31 07:40:39 +0800

    Create ryujin_situp.py

---
+++ py/ryujin_situp.py
@@ -0,0 +1,41 @@
+import pygetwindow as gw
+import pyautogui
+import time
+import keyboard
+import threading
+import win32api
+import win32con
+import debug
+import balloontip
+
+window = gw.getWindowsWithTitle("Roblox")
+try:
+    window = window[0]
+    window.activate()
+except:
+    for i in range(3):
+        print(f"\rRoblox is not detected! Closing in {3-i} seconds...", end='')
+        time.sleep(1)
+    exit()
+
+letters = "ACEQWXZ"
+
+window.activate()
+while not keyboard.is_pressed('delete'):
+    left, top = window.left, window.top
+    width, height = window.width, window.height
+    pyautogui.click(int(left+(width/2)), int(top+(height/2)))
+    for i in letters:
+        key = f'{debug.getPath()}\\ryujin_situp\\{i}.png'
+        try:
+            location = pyautogui.locateOnScreen(key, confidence=.8)
+            if location is not None:
+                keyboard.press(i)
+                time.sleep(.1)
+                keyboard.release(i)
+                print(f'Pressed {i}')
+                break
+            else:
+                print("Err")
+        except: pass
+    time.sleep(.5)
